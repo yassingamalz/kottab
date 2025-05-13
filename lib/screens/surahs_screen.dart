16af13697ea9e1ee43f86a3e43d697b00e34ea7b
yassingamalz <yassen.gamal.yg@gmail.com>
2025-05-14 02:41:41 +0300

    Fix: import fix

--- lib/screens/surahs_screen.dart
+++ lib/screens/surahs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:kottab/models/verse_set_model.dart';
 import 'package:kottab/providers/quran_provider.dart';
